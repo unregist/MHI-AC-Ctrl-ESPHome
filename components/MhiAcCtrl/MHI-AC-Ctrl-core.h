36efdfc2457ffce19669d0ec910a3825aab9d2d4
unregist <22579156+unregist@users.noreply.github.com>
2024-11-13 08:34:24 +1000

    Update MHI-AC-Ctrl-core.h
    // pin definitions for a bodge job ESP32 (non C3) I had lying around
    #define SCK_PIN  21
    #define MOSI_PIN 22
    #define MISO_PIN 23

--- components/MhiAcCtrl/MHI-AC-Ctrl-core.h
+++ components/MhiAcCtrl/MHI-AC-Ctrl-core.h
@@ -32,9 +32,13 @@ const byte opdata[][2] PROGMEM = {
 #define minTimeInternalTroom 5000              // minimal time in ms used for Troom internal sensor changes for publishing to avoid jitter 
 
 // pin defintions
-#define SCK_PIN  14
-#define MOSI_PIN 13
-#define MISO_PIN 12
+//#define SCK_PIN  14
+//#define MOSI_PIN 13
+//#define MISO_PIN 12
+// pin definitions for a bodge job ESP32 (non C3) I had lying around
+#define SCK_PIN  21
+#define MOSI_PIN 22
+#define MISO_PIN 23
 
 // constants for the frame
 #define SB0 0
