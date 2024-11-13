688e721df9dd787febef134fae934b92c5d27dff
unregist <22579156+unregist@users.noreply.github.com>
2024-11-14 06:04:48 +1000

    Update MHI-AC-Ctrl-core.h

--- components/MhiAcCtrl/MHI-AC-Ctrl-core.h
+++ components/MhiAcCtrl/MHI-AC-Ctrl-core.h
@@ -37,7 +37,7 @@ const byte opdata[][2] PROGMEM = {
 //#define MISO_PIN 12
 // Pin Definitions for my custom board ESP32-C3 v0.2 and below
 #define SCK_PIN  6
-#define MOSI_PIN 7
+#define MOSI_PIN 10
 #define MISO_PIN 2
 
 // constants for the frame
