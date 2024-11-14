18d04931a014d943b6d7fe48c1ffff64ec7c9727
unregist <22579156+unregist@users.noreply.github.com>
2024-11-14 16:37:39 +1000

    Update MHI-AC-Ctrl-core.h

--- components/MhiAcCtrl/MHI-AC-Ctrl-core.h
+++ components/MhiAcCtrl/MHI-AC-Ctrl-core.h
@@ -35,10 +35,10 @@ const byte opdata[][2] PROGMEM = {
 //#define SCK_PIN  14
 //#define MOSI_PIN 13
 //#define MISO_PIN 12
-// pin definitions for a bodge job ESP32 (non C3) I had lying around
-#define SCK_PIN  21
-#define MOSI_PIN 22
-#define MISO_PIN 23
+// pin definitions for a ESP32-C3 prototype v0.4
+#define SCK_PIN  10
+#define MOSI_PIN 7
+#define MISO_PIN 3
 
 // constants for the frame
 #define SB0 0
